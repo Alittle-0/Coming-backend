903ec1f090f1e66aeee94096e93bc13b3d21b42a
Alittle-0 <phihungdang1133@gmail.com>
2025-07-25 12:57:11 +0700

    add cors rules

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors({
-  origin: ['https://app-react-drab.vercel.app', 'http://localhost:3000'], // Your React app URL
+  origin: ['https://app-react-drab.vercel.app', 'http://localhost:3000', 'https://coming-server.vercel.app'], // Your React app URL
   credentials: true
 }));
 
